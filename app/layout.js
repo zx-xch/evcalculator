cc380c14b51e06b536a4f476fa544408cf13701d
Zach Danielson <unconfigured@null.spigotmc.org>
2024-06-12 20:47:25 -0500

    Favicon change

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "JimmyPicks EV Calculator",
+  title: "PandaPicks EV Calculator",
   description: "Calculate your expected value for DFS",
 };
 
